c4e2066f378a4ad348216978c4aa8fbb965219b8
akameco <akameco.t@gmail.com>
2016-08-17 08:11:59 +0900

    Update

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 'use strict';
 const mlw4 = require('manga-life-win-4');
 
-module.exports = () => {
-	const poputeLinks = [
-		'http://mangalifewin.takeshobo.co.jp/rensai/popute/',
-		'http://mangalifewin.takeshobo.co.jp/rensai/popute2/'
-	];
+const poputeLinks = [
+	'http://mangalifewin.takeshobo.co.jp/rensai/popute/',
+	'http://mangalifewin.takeshobo.co.jp/rensai/popute2/'
+];
 
-	return Promise.all(poputeLinks.map(mlw4)).then(links => {
-		return Array.prototype.concat.apply([], links);
-	});
-};
+module.exports = () =>
+	Promise.all(poputeLinks.map(mlw4))
+		.then(links => Array.prototype.concat.apply([], links));
